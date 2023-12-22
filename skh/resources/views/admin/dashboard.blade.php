dc85114d49dc34afd372fab65ac3b95013627f28
isandeepkumar30 <sandeep@vismaad.com>
2023-12-22 18:19:25 +0530

    test

--- skh/resources/views/admin/dashboard.blade.php
+++ skh/resources/views/admin/dashboard.blade.php
@@ -12,14 +12,14 @@
     <title>Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ env('APP_URL') }}storage/admin/assets/fontawesome-free/css/all.min.css" rel="stylesheet"
+    <link href="{{ env('APP_FILE_URL') }}storage/admin/assets/fontawesome-free/css/all.min.css" rel="stylesheet"
         type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ env('APP_URL') }}storage/admin/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ env('APP_FILE_URL') }}storage/admin/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -129,21 +129,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ env('APP_URL') }}storage/admin/assets/jquery/jquery.min.js"></script>
-    <script src="{{ env('APP_URL') }}storage/admin/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/jquery/jquery.min.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ env('APP_URL') }}storage/admin/assets/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ env('APP_URL') }}storage/admin/assets/js/sb-admin-2.min.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="{{ env('APP_URL') }}storage/admin/assets/chart.js/Chart.min.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="{{ env('APP_URL') }}storage/admin/assets/js/demo/chart-area-demo.js"></script>
-    <script src="{{ env('APP_URL') }}storage/admin/assets/js/demo/chart-pie-demo.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/js/demo/chart-area-demo.js"></script>
+    <script src="{{ env('APP_FILE_URL') }}storage/admin/assets/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
