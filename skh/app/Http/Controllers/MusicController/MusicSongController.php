f51cd1fd9ba6aa561a55ec0ac7abfae4852182c2
isandeepkumar30 <sandeep@vismaad.com>
2024-01-29 10:56:42 +0530

    url rather than ulpord in music section

--- skh/app/Http/Controllers/MusicController/MusicSongController.php
+++ skh/app/Http/Controllers/MusicController/MusicSongController.php
@@ -22,27 +22,12 @@ class MusicSongController extends Controller
     }
     public function store(Request $request)
     {
-
         $musicSong = new MusicSong;
         $musicSong->musicid = $request->input('musicid');
         $musicSong->song_name = $request->input('song_name');
         $musicSong->song_size = $request->input('song_size');
         $musicSong->song_duration = $request->input('song_duration');
-
-        if ($request->hasFile('song_path')) {
-            $file = $request->file('song_path');
-            $fileName = time() . '.' . $file->getClientOriginalExtension();
-            $folderName = 'UplordMusicSongfolder';
-            $path = public_path($folderName);
-            $upload = $file->move($path, $fileName);
-
-            if ($upload) {
-                $musicSong->song_path = $folderName . '/' . $fileName;
-            }
-        }
-
-
-
+        $musicSong->song_path = $request->input('song_path');
         $musicSong->save();
         return redirect('music_song_list')->with('status', 'Student Added Successfully');
     }
@@ -61,21 +46,9 @@ class MusicSongController extends Controller
         $musicSong->song_name = $request->input('song_name');
         $musicSong->song_size = $request->input('song_size');
         $musicSong->song_duration = $request->input('song_duration');
-
-        if ($request->hasFile('song_path')) {
-            $file = $request->file('song_path');
-            $fileName = time() . '.' . $file->getClientOriginalExtension();
-            $folderName = 'UplordMusicSongfolder';
-            $path = public_path($folderName);
-            $upload = $file->move($path, $fileName);
-
-            if ($upload) {
-                $musicSong->song_path = $folderName . '/' . $fileName;
-            }
-        }
-
+        $musicSong->song_path = $request->input('song_path');
         $musicSong->update();
-        return redirect()->back()->with('status', 'Student Updated Successfully');
+        return redirect('music_song_list')->with('status', 'Student Added Successfully');
     }
 
     public function destroy($id)
--- skh/resources/views/MusicScreen/MusicSongFolder/editMusicSong.blade.php
+++ skh/resources/views/MusicScreen/MusicSongFolder/editMusicSong.blade.php
@@ -1,89 +1,73 @@
 @extends('Layouts.master')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            @if (session('status'))
-            <h6 class="alert alert-success">{{ session('status') }}</h6>
-            @endif
-            <div class="card">
-                <div class="card-header">
-                    <h4>Edit & Update Music Song
-                        <a href="{{ url('music_song_list') }}" class="btn btn-danger float-end">BACK</a>
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <form action="{{ url('update_music_song_list/' . $musicSong->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="mb-3">
-                            <label for="musicid" class="form-label">Music Song</label>
-                            <select class="form-control select2" name="musicid" required id="musicid">
-                                <option value="option_select">Choose a Music Song</option>
-                                @foreach ($music as $item)
-                                <option value="{{ $item->id }}" @if ($item->id == $musicSong->musicid) selected @endif>
-                                    {{ $item->title }}
-                                </option>
-                                @endforeach
-                            </select>
-                        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                @if (session('status'))
+                    <div class="alert alert-success">{{ session('status') }}</div>
+                @endif
+                <div class="card">
+                    <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+                        <h4> <i style="margin-right: 10px;" class="fas fa-music"></i>Edit & Update Music Song</h4>
+                        <a href="{{ url('music_song_list') }}" class="btn btn-danger">
+                            <i class="fas fa-arrow-left"></i> Back
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ url('update_music_song_list/' . $musicSong->id) }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
 
-                        <div>
-                            <div class="form-group mb-3">
-                                <label for="">Song Name </label>
-                                <input type="text" name="song_name" class="form-control" value="{{ $musicSong->song_name }}" required>
+                            <div class="mb-3">
+                                <label for="musicid" class="form-label">Music Song</label>
+                                <select class="form-control select2" name="musicid" required id="musicid">
+                                    <option value="option_select">Choose a Music Song</option>
+                                    @foreach ($music as $item)
+                                        <option value="{{ $item->id }}"
+                                            @if ($item->id == $musicSong->musicid) selected @endif>
+                                            {{ $item->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
-                            <div class="form-group mb-3">
-                                <label for="">Song Size </label>
-                                <input type="text" name="song_size" class="form-control" value="{{ $musicSong->song_size }}" required readonly>
+
+                            <div>
+                                <div class="form-group mb-3">
+                                    <label for="song_name">Song Name </label>
+                                    <input type="text" name="song_name" class="form-control"
+                                        value="{{ $musicSong->song_name }}">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="song_size">Song Size </label>
+                                    <input type="text" name="song_size" class="form-control"
+                                        value="{{ $musicSong->song_size }}">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="song_path">Song Link</label>
+                                    <input type="text" name="song_path" class="form-control"
+                                        value="{{ $musicSong->song_path }}">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="song_duration">Song Duration Link</label>
+                                    <input type="text" name="song_duration" class="form-control"
+                                        value="{{ $musicSong->song_duration }}">
+                                </div>
                             </div>
-                            <div class="col-md-6 mb-3">
-                                <label for="song_path">Song File</label>
-                                <input type="file" class="form-control-file" id="song_path" name="song_path" required value="{{ $musicSong->song_path }}">
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fas fa-save"></i> Update Music Song
+                                </button>
                             </div>
-                            <div class="form-group mb-3">
-                                <label for="">Song Duration Link</label>
-                                <input type="text" name="song_duration" class="form-control" value="{{ $musicSong->song_duration }}" required readonly>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <button type="submit" class="btn btn-primary">Update Music Song</button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
-
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('input[type="file"]').change(function() {
-            var file = this.files[0];
-            var fileSize = (file.size / 1024 / 1024).toFixed(2);
-            if (file.type.startsWith('audio/')) {
-                var audio = document.createElement('audio');
-                audio.onloadedmetadata = function() {
-                    var duration = audio.duration;
-                    $('input[name="song_duration"]').val(duration + ' seconds');
-                };
-                audio.src = URL.createObjectURL(file);
-            } else {
-                $('input[type="file"]').val('');
-                alert('Please select an audio file.');
-                $('input[name="song_duration"]').val('NA');
-                $('input[name="song_size"]').val('NA');
-            }
-            if (file.type.startsWith('audio/')) {
-                $('input[name="song_size"]').val(fileSize + ' MB');
-            } else {
-                $('input[name="song_size"]').val('NA');
-            }
-        });
-    });
-
-</script>
